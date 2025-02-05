96bea4e48085fe2f0ad073724700ab9dac7cedf9
Gerrid Rose <gerrid.rose@gmail.com>
2025-02-05 10:42:28 -0700

    Working on fetching categories to fill the NavBar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import {useState} from 'react'
-import reactLogo from '/react.svg'
-import viteLogo from '/vite.svg'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import React from 'react';
 import Header from "./components/Header.tsx";
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,27 @@
-import { Link } from 'react-router-dom';
+import axios from 'axios';
+import {useQuery} from "react-query";
+
+const fetchCategories = async () => {
+    const response = await axios.get('http://localhost:8080/inventoryService/categories');
+    console.log("yo, here you are " + response.data);
+    return response.data;
+}
 
 export default function NavBar() {
+    const { data } = useQuery('categories', fetchCategories);
+
     return (
         <nav className="bg-amber-700 text-white py-2">
             <div className="container mx-auto flex justify-between items-center">
                 <div className="flex space-x-4">
-                    <Link to="/shop" className="hover:text-gray-400">Custom Shirts</Link>
-                    <Link to="/customize" className="hover:text-gray-400">Custom Hats</Link>
-                    <Link to="/about" className="hover:text-gray-400">Custom Drinkware</Link>
+                    <ul className="flex items-center space-x-2">
+                        {data.categories.map((category) => (
+                            <li key={category.category} />
+                        ))}
+                    </ul>
+                    {/*<Link to="/shop" className="hover:text-gray-400">Custom Shirts</Link>*/}
+                    {/*<Link to="/customize" className="hover:text-gray-400">Custom Hats</Link>*/}
+                    {/*<Link to="/about" className="hover:text-gray-400">Custom Drinkware</Link>*/}
                 </div>
             </div>
         </nav>
